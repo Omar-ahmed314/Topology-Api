8d564d25baa598f215080f2b175896ef64d6ff42
omar-ahmed314 <omar.ahmed314@hotmail.com>
2022-01-01 09:03:16 +0200

    add the topology javadoc

--- New folder/src/project2/Topology.java
+++ New folder/src/project2/Topology.java
@@ -9,6 +9,9 @@ import java.util.Scanner;
 import com.github.cliftonlabs.json_simple.JsonArray;
 import com.github.cliftonlabs.json_simple.JsonObject;
 
+/**
+ * Class that represent the topology and its components
+ * */
 public class Topology {
 	private String mTopologyId;
 	private ArrayList<Component> mComponentList;
@@ -34,6 +37,9 @@ public class Topology {
 		this.mComponentList = mComponentList;
 	}
 	
+	/**
+	 * Print the data of the resistor in ordered manner
+	 * */
 	public void print() {
 		System.out.println(mTopologyId);
 		for(Component component : mComponentList ) {
@@ -41,6 +47,10 @@ public class Topology {
 		}
 	}
 	
+	/**
+	 * reads the topology data stored in the memory file and assign its data itself
+	 * @param fileName the scanner of the memory file that read the file line by line or word by word
+	 * */
 	public void readFile(Scanner fileName) {
 		mTopologyId = fileName.next();
 		int numberOfComponents = Integer.parseInt(fileName.next());
@@ -63,6 +73,10 @@ public class Topology {
 		}
 	}
 	
+	/**
+	 * write the data of himself into the memory file with organized pattern
+	 * @param fileName The name of the memory file
+	 * */
 	public void writeFile(FileWriter fileName) {
 		try {
 			fileName.append(mTopologyId + " " + mComponentList.size() + "\n");
@@ -75,6 +89,10 @@ public class Topology {
 		}
 	}
 	
+	/**
+	 * read the data from the Json file and assign its data into a topology object
+	 * @param ob The json object of the json file
+	 * */
 	public void readJson(JsonObject ob) {
 		mTopologyId = (String)ob.get("id");
 		JsonArray components = (JsonArray)ob.get("components");
@@ -96,6 +114,11 @@ public class Topology {
 			}
 		}
 	}
+	
+	/**
+	 * write the data of himself into the Json file with organized pattern
+	 * @param ob The Json object of the Json file
+	 * */
 	public void writeJson(JsonObject ob) {
 		ob.put("id", mTopologyId);
 		JsonArray components = new JsonArray();
@@ -105,6 +128,12 @@ public class Topology {
 		}
 		
 	}
+	
+	/**
+	 * Get the components at a specific node
+	 * @param nodeId The Id of the node
+	 * @return component list, share the same common node
+	 * */
 	public ArrayList<Component> getComponentsAtNode(String nodeId){
 		ArrayList<Component> list = new ArrayList<>();
 		for(Component component: mComponentList) {
